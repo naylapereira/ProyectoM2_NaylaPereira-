9ce215ede31a624ea3bd8458d59af57fa54a051e
Nayla Pereira <naylapereira06@gmail.com>
2026-07-14 03:22:56 -0300

    configure servidor para deployment en Railway

--- src/server.js
+++ src/server.js
@@ -11,10 +11,10 @@ async function startServer() {
 
     console.log("Conexion con PostgreSQL exitosa");
 
-    app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
-} catch (error) {
+  } catch (error) {
     console.error("Error al conectar con PostgreSQL");
     console.error(error.message);
     process.exit(1);
